ef2f09091b6baec7a8c9dc231aa97b6a360f69e0
Ruslan Sibogatov <ruslansibo163@gmail.com>
2025-04-18 02:57:20 +0300

    исправлены ошибки

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,10 +9,10 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '51.250.18.244',
     'sprint-16.ru',
-    '127.0.0.1',
+    '51.250.18.244',
     'localhost',
+    '127.0.0.1'
 ]
 
 # Application definition
